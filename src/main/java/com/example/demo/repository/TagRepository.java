d62c8dc61c80128a96fbf3069bc05ffa68ed9394
docafavarato <docafavarato@gmail.com>
2024-05-04 00:07:55 -0300

    feat: add findTrending

--- src/main/java/com/example/demo/repository/TagRepository.java
+++ src/main/java/com/example/demo/repository/TagRepository.java
@@ -1,5 +1,8 @@
 package com.example.demo.repository;
 
+import java.util.List;
+
+import org.springframework.data.mongodb.repository.Aggregation;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
@@ -8,4 +11,10 @@ import com.example.demo.domain.Tag;
 @Repository
 public interface TagRepository extends MongoRepository<Tag, String> {
 	public Tag findByName(String name);
+	
+	@Aggregation(pipeline = {
+			"{ $sort: { timesUsed: -1 } }",
+			"{ $limit: ?0 }"
+	})
+	public List<Tag> findTrending(int limit);
 }
